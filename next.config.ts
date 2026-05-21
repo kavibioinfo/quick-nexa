54aefffc393535d7ed9319475d1e9e40c00eaaf1
Avinash <kavibioinfo@gmail.com>
2026-05-21 13:51:09 +0530

    secured clean programmatic sub-routing config matrix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-    ],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'header',
+            key: 'host',
+            value: 'onboard.ayushnexa.com',
+          },
+        ],
+        destination: '/onboard',
+      },
+    ];
   },
 };
 
